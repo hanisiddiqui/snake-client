fdcd019299d6aed09a39a321f14f9d71c6a4c2c1
Hani Siddiqui <hani.siddiqui@gmail.com>
2022-09-01 21:46:50 +0000

    Adjusted the exports to be object keys

---
+++ input.js
@@ -0,0 +1,17 @@
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.on("data", handleUserInput);
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+
+const handleUserInput = function () {
+  // your code here
+  if ('\u0003') {
+    process.exit();
+  }
+};
+
+module.exports = {setupInput};
--- play.js
+++ play.js
@@ -1,27 +1,9 @@
 const net = require("net");
-const connect = require('./client.js');
-
-// setup interface to handle user input from stdin
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.on("data", handleUserInput);
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  return stdin;
-};
-
-const handleUserInput = function () {
-  // your code here
-  if ('\u0003') {
-    process.exit();
-  }
-};
-
-
-
-setupInput();
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
 console.log("Connecting ...");
 connect();
+
+setupInput();
+
