507f359ff60749598c41dcaff940e7638b51082b
Hani Siddiqui <hani.siddiqui@gmail.com>
2022-09-01 21:33:02 +0000

    Added terminate command

--- play.js
+++ play.js
@@ -1,8 +1,27 @@
 const net = require("net");
 const connect = require('./client.js');
 
-// establishes a connection with the game server
+// setup interface to handle user input from stdin
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.on("data", handleUserInput);
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+
+const handleUserInput = function () {
+  // your code here
+  if ('\u0003') {
+    process.exit();
+  }
+};
+
+
+
+setupInput();
 
 console.log("Connecting ...");
 connect();
